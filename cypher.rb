3d5a96603f0c6aea83e9b806e4d42fc273efa668
Anikram <4nikram@gmail.com>
2017-11-07 11:57:05 +0300

    Version 1 (small fixtures)

--- cypher.rb
+++ cypher.rb
@@ -19,10 +19,10 @@ user_option = STDIN.gets.chomp.to_i
 while user_option do
 
   if user_option == 1
-    puts "Ваша шифрованная строка в MD5: " + Digest::MD5.hexdigest(user_input) + "."
+    puts "Ваша шифрованная строка в MD5: \n" + Digest::MD5.hexdigest(user_input) + "."
     break
   elsif user_option == 2
-    puts "Ваша шифрованная строка в SHA1:" + Digest::SHA1.hexdigest(user_input) + "."
+    puts "Ваша шифрованная строка в SHA1:\n" + Digest::SHA1.hexdigest(user_input) + "."
     break
   else
     puts "Выберите метод шифрования из списка."
